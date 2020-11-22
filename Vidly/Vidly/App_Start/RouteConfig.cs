56557123fa922e8e28e8650c64e1fb0af019bb50
David <david.joset@yahoo.fr>
2020-11-22 14:24:03 -0500

    Add movie and customer dto, add auto mapper, create api for movie and customer

--- Vidly/Vidly/App_Start/RouteConfig.cs
+++ Vidly/Vidly/App_Start/RouteConfig.cs
@@ -15,14 +15,6 @@ namespace Vidly
 
             routes.MapMvcAttributeRoutes();
 
-            //routes.MapRoute(
-            //    "MoviesByReleaseDate",
-            //    "movie/released/{year}/{month}",
-            //     new { controller = "Movie", action = "ByReleaseDate"},
-            //     new {year = @"\d{4}", month = @"\d{2}"}
-            //);
-
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
